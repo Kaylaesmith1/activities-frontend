6c76788f5de229705ff8546ce022eb3db06f07f7
Kayla S <kaylaesmith1@gmail.com>
2023-06-21 16:01:40 +0000

    Edit CSS of navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,11 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import logo from "../assets/iowa-logo.png";
+import styles from "../styles/NavBar.module.css";
 
 
 const NavBar = () => {
-  return <Navbar expand="md" fixed='top'><Container>
+  return <Navbar className={styles.NavBar} expand="md" fixed='top'><Container>
   <Navbar.Brand>
     <img src={logo} alt="logo" height="45" />
   </Navbar.Brand>
@@ -16,7 +17,7 @@ const NavBar = () => {
       <Nav.Link><i className="fas fa-home"></i> Home</Nav.Link>
       <Nav.Link><i className="fas fa-sign-in-alt"></i> Sign In</Nav.Link>
       <Nav.Link><i className="fas fa-user-plus"></i> Sign Up</Nav.Link>
-
+      
     </Nav>
 
   </Navbar.Collapse></Container>
