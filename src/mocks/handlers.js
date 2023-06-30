7396313011b1089946381f21bcf00055b5e4033e
Kayla S <kaylaesmith1@gmail.com>
2023-06-30 19:51:10 +0000

    BUG: troubleshoot testing failures for NavBar.test.js

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,21 +1,22 @@
-import { rest } from msw
+import { rest } from "msw";
 
-const baseURL = "https://activities-backend-api-a2cb7e703660.herokuapp.com/"
+const baseURL = "https://activities-backend-api-a2cb7e703660.herokuapp.com/";
 
 export const handlers = [
-    rest.get(`$(baseURL)dj-rest-auth/user/`, (req, res, ctx) => {
-        return res(ctx.json({
-            "pk": 4,
-            "username": "Oliver",
-            "email": "",
-            "first_name": "",
-            "last_name": "",
-            "profile_id": 4,
-            "profile_image": "https://res.cloudinary.com/kesmith/image/upload/v1/media/images/Screen_Shot_2023-06-30_at_15.41.31_e2nwis"
-            }),
-            rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
-                return res(ct.status(200));
-            }),
-            );
-    }),
+  rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        "pk": 4,
+        "username": "Oliver",
+        "email": "",
+        "first_name": "",
+        "last_name": "",
+        "profile_id": 4,
+        "profile_image": "https://res.cloudinary.com/kesmith/image/upload/v1/media/images/Screen_Shot_2023-06-30_at_15.41.31_e2nwis"
+        })
+    );
+  }),
+  rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
 ];
