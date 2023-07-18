8cad664b389b6936812d8534858960a2447f7b7e
Kayla S <kaylaesmith1@gmail.com>
2023-07-18 16:09:11 +0000

    Troubleshoot-remove 'back to top' button. Causing errors.

--- src/App.js
+++ src/App.js
@@ -23,9 +23,10 @@ import UsernameForm from "./pages/profiles/UsernameForm";
 import UserPasswordForm from "./pages/profiles/UserPasswordForm";
 import ProfileEditForm from "./pages/profiles/ProfileEditForm";
 
-import BackToTop from "./hooks/BackToTop";
-import btnStyles from "./styles/Button.module.css";
-import { Button } from "react-bootstrap";
+// BACK TO TOP BUTTON
+// import BackToTop from "./hooks/BackToTop";
+// import btnStyles from "./styles/Button.module.css";
+// import { Button } from "react-bootstrap";
 
 
 
@@ -34,10 +35,10 @@ import { Button } from "react-bootstrap";
 
 function App() {
 
-  BackToTop();
-  const handleBackToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }
+  // BackToTop();
+  // const handleBackToTop = () => {
+  //   window.scrollTo({ top: 0, behavior: 'smooth' });
+  // }
   const currentUser = useCurrentUser();
   const profile_id = currentUser?.profile_id || "";
 
@@ -91,7 +92,7 @@ function App() {
         </div>
         
         <div>
-  <Button
+  {/* <Button
     className={`${btnStyles.Button} ${btnStyles.BackToTop} fixed-bottom-5 left-7 z-50 cursor-pointer`}
     onClick={handleBackToTop}
     title="Back to Top"
@@ -100,7 +101,7 @@ function App() {
     <i className="fa-solid fa-circle-arrow-up" alt="back to top"></i>
     <br />
     <span>Back to Top</span>
-  </Button>
+  </Button> */}
 
 </div>
 
